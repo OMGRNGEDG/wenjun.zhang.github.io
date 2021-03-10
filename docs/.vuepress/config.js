485988535d72e46c33f62a743af9698a35fd845a
wenjun.zhang <17621682391@163.com>
2021-03-10 11:12:58 +0800

    page

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: 'wenjun.zhang个人网站',
+  description: 'wenjun.zhang 的个人网站',
+  base: '/article/',
+  serviceWorker: true,
+  head: [
+    ["meta", { name: "description", content: "技术文章" }]
+  ],
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+    repo: 'https://github.com/wenjun.zhang/article',
+    locales: {
+      '/': {
+        label: '简体中文',
+        selectText: '选择语言',
+        serviceWorker: {
+          updatePopup: {
+            message: '发现新内容可用',
+            buttonText: '刷新'
+          }
+        },
+        navbar: [
+          { text: '首页', link: '/' },
+          { text: '关于我', link: 'https://github.com/wenjun.zhang' }
+        ]
+      }
+    },
+  },
+}
